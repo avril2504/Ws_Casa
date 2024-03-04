85946411ffa13ac19b71d7d8bc74a3d3f95bef79
Avril2504 <avrilpino@gmail.com>
2024-03-04 11:08:59 +0100

    getters y setters

--- Actividad_12_Github_03/src/casa/Habitacion.java
+++ Actividad_12_Github_03/src/casa/Habitacion.java
@@ -4,5 +4,29 @@ public class Habitacion {
 	private String nombre;
 	private double metrosCuadrados;
 	private TipoHabitacion tipoHabitacion;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(double metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public TipoHabitacion getTipoHabitacion() {
+		return tipoHabitacion;
+	}
+	public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
+		this.tipoHabitacion = tipoHabitacion;
+	}
+	@Override
+	public String toString() {
+		return "Habitacion [nombre=" + nombre + ", metrosCuadrados=" + metrosCuadrados + ", tipoHabitacion="
+				+ tipoHabitacion + "]";
+	}
+	
 
 }
