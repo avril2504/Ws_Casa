8661115257c501e7a1c70f5ef569a740c01229f9
alesandrani <alesandrani@outlook.es>
2024-03-04 11:08:00 +0100

    setters

--- Actividad_12_Github_03/src/casa/Jardin.java
+++ Actividad_12_Github_03/src/casa/Jardin.java
@@ -3,4 +3,29 @@ package casa;
 public class Jardin {
 	private double metroCuadrado;
 	private int numeroPlantas;
+	public Jardin(double metroCuadrado, int numeroPlantas) {
+		super();
+		this.metroCuadrado = metroCuadrado;
+		this.numeroPlantas = numeroPlantas;
+	}
+	public Jardin() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Jardin [metroCuadrado=" + metroCuadrado + ", numeroPlantas=" + numeroPlantas + "]";
+	}
+	public double getMetroCuadrado() {
+		return metroCuadrado;
+	}
+	public void setMetroCuadrado(double metroCuadrado) {
+		this.metroCuadrado = metroCuadrado;
+	}
+	public int getNumeroPlantas() {
+		return numeroPlantas;
+	}
+	public void setNumeroPlantas(int numeroPlantas) {
+		this.numeroPlantas = numeroPlantas;
+	}
+	
 }
