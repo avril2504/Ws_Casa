b47c0e781419036057f0495d94f0ff808d3b64ef
Yinary_Hdz <hernandezyinary@gmail.com>
2024-03-04 11:05:40 +0100

    Creada la clase Casa

--- Actividad_12_Github_03/src/casa/Casa.java
+++ Actividad_12_Github_03/src/casa/Casa.java
@@ -1,6 +1,75 @@
 package casa;
 
 public class Casa {
-	//holaaaaaaaaa
+	
+	//Atributos
+	private Direccion direccion;
+	private double precio;
+	private boolean segundaMano;
+	private double metrosCuadrados;
+	private Jardin jardin;
+	private Habitacion habitacion;
+	
+	//To String
+	@Override
+	public String toString() {
+		return "Casa [direccion=" + direccion + ", precio=" + precio + ", segundaMano=" + segundaMano
+				+ ", metrosCuadrados=" + metrosCuadrados + ", jardin=" + jardin + ", habitacion=" + habitacion + "]";
+	}
+	
+	//Constructor
+	public Casa(Direccion direccion, double precio, boolean segundaMano, double metrosCuadrados, Jardin jardin,
+			Habitacion habitacion) {
+		super();
+		this.direccion = direccion;
+		this.precio = precio;
+		this.segundaMano = segundaMano;
+		this.metrosCuadrados = metrosCuadrados;
+		this.jardin = jardin;
+		this.habitacion = habitacion;
+	}
+	
+	//Setters and Getters
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public boolean isSegundaMano() {
+		return segundaMano;
+	}
+	public void setSegundaMano(boolean segundaMano) {
+		this.segundaMano = segundaMano;
+	}
+	public double getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(double metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public Jardin getJardin() {
+		return jardin;
+	}
+	public void setJardin(Jardin jardin) {
+		this.jardin = jardin;
+	}
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+	
+	//Metodos:
+	
+	
+	
 
 }
